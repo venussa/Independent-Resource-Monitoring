e0f42c2d4ee2596b93b00eadd205b88008fec286
Steave Roger <steavenroger@gmail.com>
2021-04-24 18:24:26 +0700

    New Data

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Installation
+1. npm install
+2. npm run serve
+
+# Functional File
+1. Home = /platform-api-test/src/views/home.vue
+2. Node List = /platform-api-test/src/views/node.vue
+3. Sensor List = /platform-api-test/src/views/sensor.vue
+4. Sensor Detail = /platform-api-test/src/views/detail.vue
+5. Print Page = /platform-api-test/src/views/html2pdf.vue
+
+# Note
+if something wrong during the npm installation, you can use the direct link to download node_modules at the following link
+https://drive.google.com/file/d/1E_ebvVBjoirMZyPYam5V55rCMREzX4vO/view?usp=sharing
---
+++ src/views/sensor.vue
@@ -0,0 +1,256 @@
+<!-- eslint-disable-->
+<template>
+    <div>
+      <b-col md="12" class="header">
+        <p style="margin-top: 10px;">Independent Monitoring System</p>
+        <img class="d-none d-sm-block" :src="require('@/assets/images/logo/logo.png')">
+      </b-col>
+      <b-container class="container">
+        <b-row>
+        <b-col md="8" offset-md="1">
+          <div style="margin-bottom: 20px">
+            <feather-icon icon="HomeIcon" color="#0a8de2" size="18" style="position: absolute" />
+            <p style="margin-left: 20px;">
+              <b-link :to="{ name: 'home' }">Home</b-link> / 
+              <b-link :to="{ name: 'node' }">Node List</b-link> / 
+              Sensor List
+            </p>
+          </div>
+          <b-card class="box">
+            <b-row>
+              <!-- title -->
+              <h1 class="page-title">Sensor List</h1>
+              <div class="col-md-6">
+                <div style="margin-left: -10px">
+                  Show
+                  <v-select
+                    v-model="perpage"
+                    :options="option_perpage"
+                    :components="{ Deselect, OpenIndicator }"
+                    style="display:inline-block;min-width:90px;max-width:auto"
+                    @input="loadPerpage"
+                  >
+                    <slot name="no-options">Sorry, no matching options.</slot>
+                  </v-select>
+                  entries
+                </div>
+              </div>
+              <div class="col-md-6" style="text-align: right; padding-right:0px;">
+                Search: 
+                <input v-model="filter" class="form-control" style="width: 200px; display: inline-block; margin-left: 10px;" placeholder="keyword ..." />
+              </div>
+              <div class="col-md-12" style="height: 20px" />
+              <!-- datatable -->
+              <b-table striped hover
+                :items="items"
+                :fields="fields"
+                :current-page="currentPage"
+                :per-page="perpage"
+                :filter="filter"
+                style="border-bottom: 1px #eee solid"
+              >
+                <template #cell(name)="data" aria-colspan="6" >
+                  <div class="text-nowrap">
+                    {{ data.item.name }}
+                  </div>
+                </template>
+
+                <template #cell(description)="data" aria-colspan="6" >
+                  <div class="text-nowrap">
+                    {{ data.item.description === null ? 'No description' : data.item.description }}
+                  </div>
+                </template>
+
+                <template #cell(api_key)="data" aria-colspan="6" >
+                  <div class="text-nowrap">
+                    {{ data.item.api_key }}
+                  </div>
+                </template>
+
+                <template #cell(action)="data" aria-colspan="6">
+                  <div class="text-nowrap">
+                    <b-link class="btn btn-primary" :to="{ name: 'detail', params: { 'n_id': $route.params.n_id, 's_id': data.item.id }}">View Detail</b-link>
+                  </div>
+                </template>
+
+              </b-table>
+
+              <p v-if="rows < 1" class="loading">
+                Please Wait ...
+              </p>
+
+              <!-- pagination -->
+              <div class="col-md-12" style="height:10px"></div>
+              <div class="col-md-6">
+                <p style="margin-top: 10px">Show {{first_entries}} to {{ last_entries }} of {{ items.length }} entries</p>
+                <p style="margin-top: -10px">Total sensor : {{ items.length }}</p>
+              </div>
+              <b-col md="6">
+                <b-pagination
+                  v-model="currentPage"
+                  :total-rows="rows"
+                  base-url="#"
+                  :per-page="perpage"
+                  :align="selected"
+                  @change="loadPage"
+                />
+              </b-col>
+            </b-row>
+          </b-card>
+
+        </b-col>
+        <div class="col-md-2">
+          <i class="solar-right-icon" style="margin-top: 100%; margin-left: 30px;" />
+        </div>
+        </b-row>
+      </b-container>
+    </div>
+</template>
+
+<script>
+
+import {
+  BTable,
+  BContainer,
+  BCol,
+  BRow,
+  BCard,
+  BPagination,
+  BLink,
+} from 'bootstrap-vue'
+
+import axios from 'axios'
+import httpApi from '@/main'
+import FeatherIcon from '@/@core/components/feather-icon/FeatherIcon.vue'
+import vSelect from 'vue-select'
+
+export default {
+  components: {
+    BCol,
+    BRow,
+    BTable,
+    BContainer,
+    BCard,
+    FeatherIcon,
+    BPagination,
+    BLink,
+    vSelect,
+  },
+  data() {
+    return {
+      // axios response storage
+      api: [], // api storage untuk menyimpan data axios response sensor detail
+      apikey: httpApi.ApiKey, // data api key
+
+      // konfigurasi table
+      option_perpage: [10, 20, 50, 100],
+      Deselect: {
+        render: createElement => createElement('span', ''),
+      },
+      OpenIndicator: {
+        render: createElement => createElement('span', { class: 'dropdown-icon' }),
+      },
+
+      // table field data
+      fields: [
+        { key: 'id', label: 'Id', sortable: true },
+        { key: 'name', label: 'Name', sortable: true },
+        { key: 'data_type', label: 'Data Type', sortable: true },
+        { key: 'updated_at', label: 'Time Create', sortable: true },
+        { key: 'action', label: 'Action' },
+      ],
+
+      // table value
+      items: [], // item storage untuk list data pada table
+      first_entries: 0,
+      last_entries: 0,
+      filter: '',
+
+      // pagination setting
+      selected: 'right', // mengatur posisi penomoran halaman
+      currentPage: 1, // halaman aktif table
+      perpage: 10, // jumlah data yang ditampilkan untuk tiap halaman pada table
+      rows: 0, // banyaknya data yang di tampilkan pada table
+    }
+  },
+  created() {
+    const http = axios.get(`${httpApi.host}/app/node/${this.$route.params.n_id}?api_key=${httpApi.ApiKey}&with_sensors=true`, {
+      headers: {
+        Authorization: sessionStorage.getItem('token'),
+      },
+    })
+
+    http.then(response => {
+      this.api = response.data
+    })
+
+    http.finally(() => {
+      this.items = this.api.data.sensors
+      this.rows = this.api.data.sensors.length
+      for (let x = 0; x < this.rows; x += 1) {
+        const datesplit = this.items[x].updated_at.split('T')
+        const getdate = this.dateConvert(datesplit[0], 1)
+        const timesource = datesplit[1].split('.')
+        const gettime = this.timeConvert(timesource[0], 1)
+        this.items[x].updated_at = `${getdate}. ${gettime}`
+      }
+      // entries calculation
+      this.updateState()
+    })
+  },
+  methods: {
+    // change trigger for data perpage display
+    loadPerpage(page) {
+      this.perpage = page
+      this.currentPage = 1
+      this.first_entries = 1
+      this.updateState()
+    },
+    // change trigger for pagination data
+    loadPage(page) {
+      this.currentPage = page
+      this.first_entries = 1
+      this.updateState()
+    },
+    // update entrie state
+    updateState() {
+      const pageTotal = Math.floor((this.items.length / this.perpage))
+      this.last_entries = this.perpage * this.currentPage
+      if (this.currentPage === (pageTotal + 1)) {
+        this.last_entries = this.items.length
+      }
+      this.first_entries = ((this.currentPage - 1) * this.perpage) + 1
+    },
+    // time convert
+    timeConvert(time, type = 0) {
+      let timeString = time
+      const H = +timeString.substr(0, 2)
+      const h = (H % 12) || 12
+      const ampm = H < 12 ? 'AM' : 'PM'
+      if (type === 1) {
+        timeString = `${h + timeString.substr(2, 3)} ${ampm}`
+      } else {
+        timeString = `${h + timeString.substr(0, 0)} ${ampm}`
+      }
+      return timeString
+    },
+    // date convert
+    dateConvert(data, type = 0) {
+      const datename = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+      const datesplit = data.split('-')
+
+      let month = ''
+      if (type > 0) {
+        month = `${datename[(parseInt(datesplit[1], 10) - 1)]} ${datesplit[2]}, ${datesplit[0]}`
+      } else {
+        month = `${datename[(parseInt(datesplit[1], 10) - 1)]} ${datesplit[2]}, `
+      }
+      return month
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import '@core/scss/vue/libs/vue-select.scss';
+</style>
